a495710ef8edeca435688ebee90bac9420eb7a01
SRV <vikneshsrv24@gmail.com>
2024-03-11 01:59:45 -0700

    update #2

--- vending machine.py
+++ vending machine.py
@@ -33,10 +33,10 @@ def get_bet():
         amount=input("How much you like to bet ? ")
         if amount.isdigit():
             amount=int(amount)
-            if amount >0:
+            if MIN_BET<=amount<MAX_BET:
                 break
             else:
-                print("Enter more than Zero !")
+                print(f"Enter more than between ${MIN_BET}-${MAX_BET} !")
         else:
             print("Enter a number...")
     return amount
@@ -46,7 +46,11 @@ def get_bet():
 def main():
     balance=deposit()
     lines=no_of_lines()
+    bet=get_bet()
+    total_bet=bet*lines
+    print(f"You are betting ${bet} on ${lines}lines. Your Total bet is equal to: ${total_bet}")
     print(balance,lines)
 
 
 
+main()
